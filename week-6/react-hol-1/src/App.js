f520dcd89c17cee3c3eac33cd27e675fa7709343
Likithgovind <likigovind@gmail.com>
2025-07-26 22:42:09 +0530

    Added Week 6 - Hands-on 1 (ReactJS HOL)

---
+++ week-6/react-hol-1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Welcome from './welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ReactJS Hands-on 1</h1>
+      <Welcome name="Likith" />
+    </div>
+  );
+}
+
+export default App;
---
+++ week-6/react-hol-1/src/welcome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Welcome(props) {
+  return <h2>Welcome to ReactJS HOL 1, {props.name}!</h2>;
+}
+
+export default Welcome;
